fe0b5a379480c08ba459c0c7880c1325e5fc6a2a
defjoy <defjoy9@users.noreply.github.com>
2025-05-11 21:47:28 +0200

    changed libraries - unplaced content

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from docx import Document
-from docx.shared import Inches
+from PIL import Image, ImageDraw, ImageFont
 from datetime import datetime
 
 full_name = input("enter you full name: ")
 
-document = Document()
-# document.add_picture('background.jpeg', width=Inches(2))
-document.add_heading('Corporation Of Nothing', 2)
-document.add_heading('Certificate of Absolute Shiet Programming SKills', 1)
-document.add_paragraph('This is to certify that')
-document.add_heading(full_name, 1)
-document.add_paragraph("For demonstrating an exceptional ability to: Write code that runs nowhere, Debug problems that don’t exist (and miss the ones that do), Master the ancient art of Googling Stack Overflow for the wrong error, And commit code so meaningless, Git itself questioned its purpose.")
-document.add_paragraph(f'Issued on {datetime.now().strftime("%d/%m/%Y")}')
-document.add_paragraph('Ms Defjoy')
-document.add_paragraph('CEO OF THE WORST PROGRAMMING SKILLS')
-document.save('certificate.docx')
+
+image = Image.open("background.png")
+draw = ImageDraw.Draw(image)
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 40)
+
+draw.text((350,100), "Corporation Of Nothing", font=font, fill="black")
+draw.text((350,150), "Certificate of Absolute Shiet Programming SKills", font=font, fill="black")
+draw.text((350,300), "This is to certify that", font=font, fill="black")
+draw.text((350,300), full_name, font=font, fill="black")
+draw.text((350,300), "For demonstrating an exceptional ability to: Write code that runs nowhere, Debug problems that don’t exist (and miss the ones that do), Master the ancient art of Googling Stack Overflow for the wrong error, And commit code so meaningless, Git itself questioned its purpose.", font=font, fill="black")
+draw.text((350,500), f'Issued on {datetime.now().strftime("%B %d, %Y")}', font=font, fill="black")
+draw.text((350,300), "Ms Defjoy", font=font, fill="black")
+draw.text((350,300), "CEO OF THE WORST PROGRAMMING SKILLS", font=font, fill="black")
+
+
+image.save("certificate.png")
